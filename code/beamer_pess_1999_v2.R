c7af2393b504f00e82c8419919f8a9b4edca1bf0
Aimee Fullerton <aimeefullerton@Aimees-MBP.hsd1.wa.comcast.net>
2023-06-15 11:07:49 -0700

    Add linear relationsip script

---
+++ code/beamer_pess_1999_v2.R
@@ -0,0 +1,53 @@
+
+# Relate egg to fry survival and peak flow as described in the references below by extracting data from plots, fitting models, and plotting
+# last updated: AH Fullerton, 6/12/2023
+
+# Beamer, E., and G. R. Pess. 1999. Effects of peak flows on chinook (Oncorhynchus tshawytscha) spawning success in two Puget Sound 
+  # river basins. Pages 67-70 in Watershed Management to Protect Declining Species. Proceedings American Water Resources Association 
+  # 1999 Annual Water Resources Conference, Seattle, WA.  
+
+# Seiler, D., S. Neuhauser, and L. Kishimoto. 2002. 2001 Skagit River wild 0+ chinook production evalutaion. State of Washington 
+  # Department of Fish and Wildlife, and Seattle City Light, Annual Report, Seattle, WA.
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Read in estimated data
+# Empirical data points from Figure 2 were extracted using http://www.graphreader.com/ and saved in the .csv file
+td <- read.csv("data/beamer_pess_1999_v2.csv")
+plot_name <- "beamer_pess_1999_v2"
+
+# X-variable
+x <- td$x
+xlb <- "Incubation flows (cfs)" # label used in plots
+
+# Y-variable
+y <- td$y
+ylb <- "Egg to migrant survival" # label used in plots
+
+# Convert response from percent to proportion
+y <- y / 100
+
+# Convert x-axis to something we can read on the plot (kcfs)
+x <- x / 1000
+x <- round(x)
+
+# Create new dataset used for predicting (extending the X-value points from a few to many within the existing range)
+xl <- round(seq(min(x), max(x), length.out = 1000))
+nd <- as.data.frame(xl); colnames(nd) <- "x" #nls() which is used below needs this to be a dataframe with named variable as expected
+
+# Fit a linear model
+(m1 <- lm(y ~ x))
+summary(m1)
+m1.pred <- predict(m1)
+cor(y, m1.pred) #test fit to see if this was an appropriate form
+m1.pred <- predict(m1, newdata = nd)
+
+
+# Plot graph (saved)
+png(paste0("plots/", plot_name, ".png"), width = 5, height = 4, units = "in", res = 300)
+plot(x, y, pch = 19, col = 1, las = 1, ylab = ylb, xlab = xlb)
+lines(xl, m1.pred, col = 4, lwd = 2)
+legend("topright", legend = c("Extracted data", "Fitted equation"), col = c(1, 4), lwd = c(NA, 2), pch = c(19, NA), bty = 'n')
+dev.off()
+
+
